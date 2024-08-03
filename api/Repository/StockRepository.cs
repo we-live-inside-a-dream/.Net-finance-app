d4cbe3b8a7b01d9a0ba8162bfbe4269ed5f3e613
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2024-08-02 20:39:28 -0600

    WIN

--- api/Repository/StockRepository.cs
+++ api/Repository/StockRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Data;
+using api.Dtos.Stock;
 using api.Interfaces;
 using api.Models;
 using Microsoft.EntityFrameworkCore; 
@@ -16,9 +17,41 @@ namespace api.Repository
         {
             _context = context;
         }
+
+        public Task<Stock> CreateAsync(Stock stockModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Stock?> DeleteAsync(int id)
+        {
+             var stockModel = await _context.Stocks.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (stockModel == null)
+            {
+                return null;
+            }
+
+            _context.Stocks.Remove(stockModel);
+
+            await _context.SaveChangesAsync();
+
+            return stockModel;
+        }
+
         public  Task<List<Stock>> GetAllAsync()
         {
             return  _context.Stocks.ToListAsync();
+        }
+
+        public Task<Stock?> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Stock?> UpdateAsync(int id, UpdateStockRequestDto stockDto)
+        {
+            throw new NotImplementedException();
         }
     }
 }
