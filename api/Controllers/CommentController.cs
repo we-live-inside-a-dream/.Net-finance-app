6d16afb28106a8cba0b140dafc951e1233d73e8a
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2024-08-05 17:20:24 -0600

    feat: comment controller, GetAll

---
+++ api/Controllers/CommentController.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Data;
+using api.Dtos.Comment;
+using api.Interfaces;
+using api.Mappers;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api/comment")]
+    [ApiController]
+    public class CommentController : ControllerBase
+    {
+        private readonly ICommentRepository _commentRepo;
+
+        public CommentController(ApplicationDBContext context, ICommentRepository commentRepo)
+        {
+            _commentRepo = commentRepo;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var comments = await _commentRepo.GetAllAsync();
+
+            var commentDto = comments.Select(c => c.ToCommentDto());
+
+            return Ok(commentDto);
+        }
+    }
+}
