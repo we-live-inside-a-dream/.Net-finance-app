61e8564f929dee4ac7434072e475192f871967bb
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2024-08-05 15:24:42 -0600

    feat: comments repository and interface, get all

--- api/Interfaces/ICommentRepository.cs
+++ api/Interfaces/ICommentRepository.cs
@@ -2,11 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using api.Models;
 
 namespace api.Interfaces
 {
-    public class ICommentRepository
+    public interface ICommentRepository
     {
-        
+        Task<List<Comment>> GetAllAsync();
+
     }
 }
