88489fba1c9639664587acdfafa3f7ffa67e2dea
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2024-08-06 22:30:22 -0600

    feat: comments get by id

--- api/Controllers/CommentController.cs
+++ api/Controllers/CommentController.cs
@@ -31,5 +31,18 @@ namespace api.Controllers
 
             return Ok(commentDto);
         }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var comment = await _commentRepo.GetByIdAsync(id);
+
+            if (comment == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(comment.ToCommentDto());
+        }
     }
 }
--- api/Interfaces/ICommentRepository.cs
+++ api/Interfaces/ICommentRepository.cs
@@ -9,6 +9,7 @@ namespace api.Interfaces
     public interface ICommentRepository
     {
         Task<List<Comment>> GetAllAsync();
+        Task<Comment?> GetByIdAsync(int id);
 
     }
 }
