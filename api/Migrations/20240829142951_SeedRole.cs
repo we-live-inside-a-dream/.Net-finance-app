b5eeab7ddad410950c002738715f85b454afe695
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2024-08-29 08:44:40 -0600

    feat: migration added user table

---
+++ api/Migrations/20240829142951_SeedRole.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "0a03264e-4967-4572-8124-3dd7f4d048d4", null, "User", "USER" },
+                    { "b4ff28b6-edd5-4661-b222-678fc197c5a3", null, "Admin", "ADMIN" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "0a03264e-4967-4572-8124-3dd7f4d048d4");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "b4ff28b6-edd5-4661-b222-678fc197c5a3");
+        }
+    }
+}
