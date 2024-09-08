3ba8d478d7949f5dacb17d6e52b75dfa11b51a0e
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2024-09-08 12:10:10 -0600

    feat: add authorize to get all stocks

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -7,6 +7,7 @@ using api.Dtos.Stock;
 using api.helper;
 using api.Interfaces;
 using api.Mappers;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -23,6 +24,7 @@ namespace api.Controllers
         }
 
         [HttpGet]
+        [Authorize]
         public async Task<IActionResult> GetAll([FromQuery] QueryObject query)
         {
             if (!ModelState.IsValid)
