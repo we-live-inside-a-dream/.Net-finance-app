b18bc20c2eff6104f3e7c9f23473b98928876593
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2024-09-19 12:38:15 -0600

    feat: added CreatAsync to portfolio Interface and Repository

--- api/Repository/PortfolioRepository.cs
+++ api/Repository/PortfolioRepository.cs
@@ -16,6 +16,14 @@ namespace api.Repository
         {
             _context = context;
         }
+
+        public async Task<Portfolio> CreateAsync(Portfolio portfolio)
+        {
+            await _context.Portfolios.AddAsync(portfolio);
+            await _context.SaveChangesAsync();
+            return portfolio;
+        }
+
         public async Task<List<Stock>> GetUserPortfolio(AppUser user)
         {
             return await _context.Portfolios.Where(u => u.AppUserId == user.Id)
@@ -28,7 +36,7 @@ namespace api.Repository
                 LastDiv = stock.Stock.LastDiv,
                 Industry = stock.Stock.Industry,
                 MarketCap = stock.Stock.MarketCap
-                
+
             }).ToListAsync();
         }
     }
