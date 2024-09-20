817aac7edafc6403000dd90e7e12ae69cec270fc
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2024-09-20 12:32:35 -0600

    chore: seperated backend files

---
+++ backend/api/Repository/CommentRepository.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Data;
+using api.Dtos.Comment;
+using api.Interfaces;
+using api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Repository
+{
+    public class CommentRepository : ICommentRepository
+    {
+        private readonly ApplicationDBContext _context;
+        public CommentRepository(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<Comment>> GetAllAsync()
+        {
+            return await _context.Comments.Include(a => a.AppUser).ToListAsync();
+        }
+
+        public async Task<Comment?> GetByIdAsync(int id)
+        {
+            return await _context.Comments.Include(a => a.AppUser).FirstOrDefaultAsync(c => c.Id == id);
+        }
+
+        public async Task<Comment> CreateAsync(Comment commentModel)
+        {
+            await _context.Comments.AddAsync(commentModel);
+            await _context.SaveChangesAsync();
+            return commentModel;
+        }
+
+        public async Task<Comment?> UpdateAsync(int id, Comment commentModel)
+        {
+            var existingComment = await _context.Comments.FindAsync(id);
+
+            if (existingComment == null)
+            {
+                return null;
+            }
+
+            existingComment.Title = commentModel.Title;
+            existingComment.Content = commentModel.Content;
+
+            await _context.SaveChangesAsync();
+
+            return existingComment;
+        }
+
+        public async Task<Comment?> DeleteAsync(int id)
+        {
+            var CommentModel = await _context.Comments.FirstOrDefaultAsync(c => c.Id == id);
+
+            if (CommentModel == null)
+            {
+                return null;
+            }
+
+            _context.Comments.Remove(CommentModel);
+
+            await _context.SaveChangesAsync();
+
+            return CommentModel;
+        }
+    }
+}
