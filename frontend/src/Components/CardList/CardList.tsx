b078cc67579a337941ecd3587dc48bbe8a5c367c
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2024-09-20 16:08:06 -0600

    chore: test, ignore commit

--- frontend/src/Components/CardList/CardList.tsx
+++ frontend/src/Components/CardList/CardList.tsx
@@ -9,7 +9,6 @@ const CardList = (props: Props) => {
       <Card companyName="Apple" ticker="AAPL" price={100} />
       <Card companyName="Microsoft" ticker="MSFT" price={200} />
       <Card companyName="Tesla" ticker="TSLA" price={300} />
-      <div>test</div>
     </div>
   );
 };
