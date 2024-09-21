0b50ca40621a5def31befe39ee25bb450abe40bb
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2024-09-21 12:00:16 -0600

    feat: reafactor
    Refactored code and brought states to App.tsx, this is done because we want to seperate smart components fr
    om dumb components for better data flow

--- frontend/src/Components/Search/Search.tsx
+++ frontend/src/Components/Search/Search.tsx
@@ -1,20 +1,23 @@
-import { ChangeEvent, useState, MouseEvent, SyntheticEvent } from "react";
+import {
+  ChangeEvent,
+  useState,
+  MouseEvent,
+  SyntheticEvent,
+  FormEvent,
+} from "react";
 import "./Search.css";
 
-type Props = {};
+interface Props {
+  onClick: (e: SyntheticEvent) => void;
+  search: string | undefined;
+  handleChange: (e: ChangeEvent<HTMLInputElement>) => void;
+}
 
-const Search: React.FC<Props> = (props: Props): JSX.Element => {
-  const [search, setSearch] = useState<string>("");
-
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setSearch(e.target.value);
-    console.log("e", e);
-  };
-
-  const onClick = (e: SyntheticEvent) => {
-    console.log(e);
-  };
-
+const Search: React.FC<Props> = ({
+  onClick,
+  search,
+  handleChange,
+}: Props): JSX.Element => {
   return (
     <div>
       <input value={search} onChange={(e) => handleChange(e)}></input>
