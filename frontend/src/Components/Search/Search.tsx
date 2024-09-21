b90efff3d7d120525e7d9161cb4338879fe693ea
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2024-09-20 18:52:22 -0600

    feat: created a simple search, not functioning for now

--- frontend/src/Components/Search/Search.tsx
+++ frontend/src/Components/Search/Search.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { ChangeEvent, useState, MouseEvent, SyntheticEvent } from "react";
 import "./Search.css";
 
 type Props = {};
@@ -6,14 +6,19 @@ type Props = {};
 const Search: React.FC<Props> = (props: Props): JSX.Element => {
   const [search, setSearch] = useState<string>("");
 
-  const onClick = (e: any) => {
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
     console.log("e", e);
   };
 
+  const onClick = (e: SyntheticEvent) => {
+    console.log(e);
+  };
+
   return (
     <div>
-      <input value={search} onChange={(e) => onClick(e)}></input>
+      <input value={search} onChange={(e) => handleChange(e)}></input>
+      <button onClick={(e) => onClick(e)} />
     </div>
   );
 };
