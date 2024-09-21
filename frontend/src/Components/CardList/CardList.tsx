3218a1de1660f170143a93c9578cd4237d49690c
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2024-09-21 12:28:39 -0600

    feat: mapping the searchResult and passing it to Card

--- frontend/src/Components/CardList/CardList.tsx
+++ frontend/src/Components/CardList/CardList.tsx
@@ -1,16 +1,26 @@
 import React from "react";
 import Card from "../Card/Card";
 import "./CardList.css";
+import { CompanySearch } from "../../company";
+import { v4 as uuidv4 } from "uuid";
 
-interface Props {}
+interface Props {
+  searchResult: CompanySearch[];
+}
 
-const CardList: React.FC<Props> = (props: Props): JSX.Element => {
+const CardList: React.FC<Props> = ({ searchResult }: Props): JSX.Element => {
   return (
-    <div>
-      <Card companyName="Apple" ticker="AAPL" price={100} />
-      <Card companyName="Microsoft" ticker="MSFT" price={200} />
-      <Card companyName="Tesla" ticker="TSLA" price={300} />
-    </div>
+    <>
+      {searchResult.length > 0 ? (
+        searchResult.map((result) => {
+          return (
+            <Card id={result.symbol} key={uuidv4()} searchResult={result} />
+          );
+        })
+      ) : (
+        <h1>No Result</h1>
+      )}
+    </>
   );
 };
 
