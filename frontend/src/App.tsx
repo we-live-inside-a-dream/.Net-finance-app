beb8fff242a1fac4fa09e814393208f6bfed8722
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2024-09-21 12:26:35 -0600

    feat: passed searchResult to carList

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -28,7 +28,7 @@ function App() {
     <div className="App">
       <Search onClick={onClick} search={search} handleChange={handleChange} />
       {serverError && <h1>{serverError}</h1>}
-      <CardList />
+      <CardList searchResult={searchResult}/>
     </div>
   );
 }
