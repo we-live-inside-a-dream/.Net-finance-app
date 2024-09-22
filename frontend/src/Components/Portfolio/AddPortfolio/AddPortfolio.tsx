e9108704e3e00aec87375af7ea26a582f8d0f265
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2024-09-21 19:50:29 -0600

    feat: addPortfolio folder location change

---
+++ frontend/src/Components/Portfolio/AddPortfolio/AddPortfolio.tsx
@@ -0,0 +1,18 @@
+import { SyntheticEvent } from "react";
+import "./AddPortfolio.css";
+
+interface Props {
+  onPortfolioCreate: (e: SyntheticEvent) => void;
+  symbol: string;
+}
+
+const AddPortfolio = ({ onPortfolioCreate, symbol }: Props) => {
+  return (
+    <form onSubmit={onPortfolioCreate}>
+      <input readOnly={true} hidden={true} value={symbol} />
+      <button type="submit">Add</button>
+    </form>
+  );
+};
+
+export default AddPortfolio;
