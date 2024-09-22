9aee5c3f6f3e398e4196adc2381d1ce3f8a3244d
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2024-09-21 18:51:35 -0600

    feat: created portfolio, not complete

---
+++ frontend/src/Components/Portfolio/Portfolio.tsx
@@ -0,0 +1,14 @@
+import "./Portfolio.css";
+import React, { SyntheticEvent } from "react";
+
+type Props = {
+  
+};
+
+const Portfolio = (props: Props) => {
+  return (
+    <div></div>
+  );
+};
+
+export default Portfolio;
