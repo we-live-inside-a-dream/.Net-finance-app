e35043bc29552f4f835eaf988c5c815024abb1bf
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2024-09-22 02:13:21 -0600

    feat: brough navbar into the app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,8 @@ import Search from "./Components/Search/Search";
 import { CompanySearch } from "./company";
 import { searchCompanies } from "./api";
 import ListPortfolio from "./Components/Portfolio/ListPortfolio/ListPortfolio";
+import Navbar from "./Components/Navbar/Navbar";
+import Hero from "./Components/Hero/Hero";
 
 function App() {
   const [search, setSearch] = useState<string>("");
@@ -44,6 +46,7 @@ function App() {
   };
   return (
     <div className="App">
+      <Navbar />
       <Search
         onSearchSubmit={onSearchSubmit}
         search={search}
