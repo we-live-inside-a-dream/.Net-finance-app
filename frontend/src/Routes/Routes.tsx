008ea0223a887314f79e6ea8bf541eaedfed0ea6
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2024-09-22 15:09:39 -0600

    feat: nested routes

--- frontend/src/Routes/Routes.tsx
+++ frontend/src/Routes/Routes.tsx
@@ -3,6 +3,8 @@ import App from "../App";
 import HomePage from "../Pages/HomePage/HomePage";
 import SearchPage from "../Pages/SearchPage/SearchPage";
 import CompanyPage from "../Pages/CompanyPage/CompanyPage";
+import IncomeStatement from "../Components/IncomeStatement/IncomeStatement";
+import CompanyProfile from "../Components/CompanyProfile/CompanyProfile";
 
 export const router = createBrowserRouter([
   {
@@ -20,6 +22,16 @@ export const router = createBrowserRouter([
       {
         path: "company/:ticker",
         element: <CompanyPage />,
+        children: [
+          {
+            path: "company-profile",
+            element: <CompanyProfile />,
+          },
+          {
+            path: "income-statement",
+            element: <IncomeStatement />,
+          },
+        ],
       },
     ],
   },
