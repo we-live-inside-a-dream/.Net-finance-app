fd27125af894d9bb03d2140f22bce23cf638b3b7
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2024-09-22 03:32:54 -0600

    feat: fetching company name

--- frontend/src/Pages/CompanyPage/CompanyPage.tsx
+++ frontend/src/Pages/CompanyPage/CompanyPage.tsx
@@ -1,9 +1,35 @@
+import { useParams } from "react-router";
 import "./CompanyPage.css";
+import { useEffect, useState } from "react";
+import { CompanyProfile } from "../../company";
+import { getCompanyProfile } from "../../api";
 
 interface Props {}
 
 const CompanyPage = (props: Props) => {
-  return <div>CompanyPage</div>;
+  const [company, setCompany] = useState<CompanyProfile>();
+
+  let { ticker } = useParams();
+
+  useEffect(() => {
+    const getProfileInit = async () => {
+      const result = await getCompanyProfile(ticker!);
+      if (typeof result !== "string") {
+        setCompany(result?.data[0]);
+      }
+    };
+    getProfileInit();
+  }, []);
+
+  return (
+    <>
+      {company ? (
+        <div>{company.companyName}</div>
+      ) : (
+        <div>Company not found!</div>
+      )}
+    </>
+  );
 };
 
 export default CompanyPage;
