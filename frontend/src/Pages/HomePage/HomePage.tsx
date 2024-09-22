a8d7ec330fd03c85005dd0145516a8f9c326b8cf
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2024-09-22 03:02:30 -0600

    feat: calling Hero into home page

--- frontend/src/Pages/HomePage/HomePage.tsx
+++ frontend/src/Pages/HomePage/HomePage.tsx
@@ -1,9 +1,14 @@
+import Hero from "../../Components/Hero/Hero";
 import "./HomePage.css";
 
 interface Props {}
 
 const HomePage = (props: Props) => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
 };
 
 export default HomePage;
