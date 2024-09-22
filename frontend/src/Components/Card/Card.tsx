487d6b8a91b4a45e6802b7ecd6aebf1609bb22ce
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2024-09-21 18:55:24 -0600

    feat: includng AddPortfolio into a Card

--- frontend/src/Components/Card/Card.tsx
+++ frontend/src/Components/Card/Card.tsx
@@ -1,12 +1,19 @@
+import { SyntheticEvent } from "react";
 import { CompanySearch } from "../../company";
+import AddPortfolio from "../AddPortfolio/AddPortfolio";
 import "./Card.css";
 
 interface Props {
   id: string;
   searchResult: CompanySearch;
+  onPortfolioCreate: (e: SyntheticEvent) => void;
 }
 
-const Card: React.FC<Props> = ({ id, searchResult }: Props): JSX.Element => {
+const Card: React.FC<Props> = ({
+  id,
+  searchResult,
+  onPortfolioCreate,
+}: Props): JSX.Element => {
   return (
     <div className="card">
       <img
@@ -22,6 +29,10 @@ const Card: React.FC<Props> = ({ id, searchResult }: Props): JSX.Element => {
       <p className="info">
         {searchResult.exchangeShortName} - {searchResult.stockExchange}
       </p>
+      <AddPortfolio
+        onPortfolioCreate={onPortfolioCreate}
+        symbol={searchResult.symbol}
+      />
     </div>
   );
 };
