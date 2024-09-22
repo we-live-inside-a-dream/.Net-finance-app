c73362121124dcb728e7c9c1872ef50f8e193ada
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2024-09-22 03:31:47 -0600

    feat: new api to get companyProfile

--- frontend/src/api.tsx
+++ frontend/src/api.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { CompanySearch } from "./company";
+import { CompanyProfile, CompanySearch } from "./company";
 
 interface SearchResponse {
   data: CompanySearch[];
@@ -14,6 +14,23 @@ export const searchCompanies = async (query: string) => {
   } catch (error) {
     if (axios.isAxiosError(error)) {
       console.log("error message: ", error.message);
+      return error.message;
+    } else {
+      console.log("unexpected error: ", error);
+      return "An unexpected error has occurred.";
+    }
+  }
+};
+
+export const getCompanyProfile = async (query: string) => {
+  try {
+    const data = await axios.get<CompanyProfile[]>(
+      `https://financialmodelingprep.com/api/v3/profile/${query}?apikey=${process.env.REACT_APP_API_KEY}`
+    );
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.log("error message from API: ", error.message);
       return error.message;
     } else {
       console.log("unexpected error: ", error);
