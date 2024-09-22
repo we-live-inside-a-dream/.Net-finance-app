4b5034138fa47c5135914995df8a0441660ea33f
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2024-09-22 02:24:44 -0600

    feat: style

--- frontend/src/Components/CardList/CardList.tsx
+++ frontend/src/Components/CardList/CardList.tsx
@@ -27,7 +27,9 @@ const CardList: React.FC<Props> = ({
           );
         })
       ) : (
-        <h1>No Result</h1>
+        <p className="mb-3 mt-3 text-xl font-semibold text-center md:text-xl">
+          No results!
+        </p>
       )}
     </>
   );
