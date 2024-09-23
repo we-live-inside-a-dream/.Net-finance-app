1bba3c828897489a50983c4ae784c7fbf5f19c14
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2024-09-23 13:44:25 -0600

    feat: passing props to RatioList

--- frontend/src/Pages/DesignPage/DesignPage.tsx
+++ frontend/src/Pages/DesignPage/DesignPage.tsx
@@ -1,8 +1,17 @@
 import RatioList from "../../Components/RatioList/RatioList";
 import Table from "../../Components/Table/Table";
+import { testIncomeStatementData } from "../../Components/Table/testData";
 import "./DesignPage.css";
 
 interface Props {}
+
+const tableConfig = [
+  {
+    label: "Market Cap",
+    render: (company: any) => company.marketCapTTM,
+    subTitle: "Total value of all a company's shares of stock",
+  },
+];
 
 const DesignPage = (props: Props) => {
   return (
@@ -12,7 +21,7 @@ const DesignPage = (props: Props) => {
         This is TradeShark's design page. This is where we will house various
         design aspects of the app
       </h2>
-      <RatioList />
+      <RatioList data={testIncomeStatementData} config={tableConfig}/>
       <Table />
     </>
   );
