247786c61d8fdaaf23c45db2b192f1aebd5e2e03
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2024-09-23 15:24:59 -0600

    feat: balancesheet component
    created BalanceSheet Component, fetching balancesheed data and displaying it in RatioList. there is something wrong with the ratioList style. figure out later

---
+++ frontend/src/Components/BalanceSheet/BalanceSheet.tsx
@@ -0,0 +1,91 @@
+import { useOutletContext } from "react-router";
+import { CompanyBalanceSheet, CompanyCashFlow } from "../../company";
+import "./BalanceSheet.css";
+import { useEffect, useState } from "react";
+import { getBalanceSheet } from "../../api";
+import RatioList from "../RatioList/RatioList";
+
+interface Props {}
+
+const config = [
+  {
+    label: <div className="font-bold">Total Assets</div>,
+    render: (company: CompanyBalanceSheet) => company.totalAssets,
+  },
+  {
+    label: "Current Assets",
+    render: (company: CompanyBalanceSheet) => company.totalCurrentAssets,
+  },
+  {
+    label: "Total Cash",
+    render: (company: CompanyBalanceSheet) => company.cashAndCashEquivalents,
+  },
+  {
+    label: "Property & equipment",
+    render: (company: CompanyBalanceSheet) => company.propertyPlantEquipmentNet,
+  },
+  {
+    label: "Intangible Assets",
+    render: (company: CompanyBalanceSheet) => company.intangibleAssets,
+  },
+  {
+    label: "Long Term Debt",
+    render: (company: CompanyBalanceSheet) => company.longTermDebt,
+  },
+  {
+    label: "Total Debt",
+    render: (company: CompanyBalanceSheet) => company.otherCurrentLiabilities,
+  },
+  {
+    label: <div className="font-bold">Total Liabilites</div>,
+    render: (company: CompanyBalanceSheet) => company.totalLiabilities,
+  },
+  {
+    label: "Current Liabilities",
+    render: (company: CompanyBalanceSheet) => company.totalCurrentLiabilities,
+  },
+  {
+    label: "Long-Term Debt",
+    render: (company: CompanyBalanceSheet) => company.longTermDebt,
+  },
+  {
+    label: "Long-Term Income Taxes",
+    render: (company: CompanyBalanceSheet) => company.otherLiabilities,
+  },
+  {
+    label: "Stakeholder's Equity",
+    render: (company: CompanyBalanceSheet) => company.totalStockholdersEquity,
+  },
+  {
+    label: "Retained Earnings",
+    render: (company: CompanyBalanceSheet) => company.retainedEarnings,
+  },
+];
+
+const BalanceSheet = (props: Props) => {
+  const ticker = useOutletContext<string>();
+  const [balanceSheet, setBalanceSheet] = useState<CompanyBalanceSheet>();
+
+  useEffect(() => {
+    const fetchBalanceSheet = async () => {
+      const result = await getBalanceSheet(ticker);
+      if (typeof result !== "string") {
+        setBalanceSheet(result?.data[0]);
+      }
+    };
+    fetchBalanceSheet();
+  }, []);
+  return (
+    <>
+      {balanceSheet ? (
+        <>
+          <RatioList config={config} data={balanceSheet} />
+        </>
+      ) : (
+        <h1>Company not found</h1>
+      )}
+    </>
+  );
+};
+
+export default BalanceSheet;
