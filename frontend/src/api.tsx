47114c1acc5fac454f2816d554b038c6f1f3e34d
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2024-09-23 14:29:08 -0600

    feat: api call for incomeStatement

--- frontend/src/api.tsx
+++ frontend/src/api.tsx
@@ -1,5 +1,10 @@
 import axios from "axios";
-import { CompanyKeyMetrics, CompanyProfile, CompanySearch } from "./company";
+import {
+  CompanyIncomeStatement,
+  CompanyKeyMetrics,
+  CompanyProfile,
+  CompanySearch,
+} from "./company";
 
 interface SearchResponse {
   data: CompanySearch[];
@@ -39,11 +44,27 @@ export const getCompanyProfile = async (query: string) => {
   }
 };
 
-// 
 export const getKeyMetrics = async (query: string) => {
   try {
     const data = await axios.get<CompanyKeyMetrics[]>(
       `https://financialmodelingprep.com/api/v3/key-metrics-ttm/${query}?apikey=${process.env.REACT_APP_API_KEY}`
+    );
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.log("error message from API: ", error.message);
+      return error.message;
+    } else {
+      console.log("unexpected error: ", error);
+      return "An unexpected error has occurred.";
+    }
+  }
+};
+
+export const getIncomeStatement = async (query: string) => {
+  try {
+    const data = await axios.get<CompanyIncomeStatement[]>(
+      `https://financialmodelingprep.com/api/v3/income-statement/${query}?limit=40&apikey=${process.env.REACT_APP_API_KEY}`
     );
     return data;
   } catch (error) {
