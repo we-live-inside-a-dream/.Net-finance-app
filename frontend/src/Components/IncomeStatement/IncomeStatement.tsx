3417f5bab9941dadd6f8ab09ba84cce7336b1db8
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2024-09-23 14:29:53 -0600

    feat: fetching incomeStatment and passing to Table

--- frontend/src/Components/IncomeStatement/IncomeStatement.tsx
+++ frontend/src/Components/IncomeStatement/IncomeStatement.tsx
@@ -1,9 +1,93 @@
+import { useOutletContext } from "react-router-dom";
+import { CompanyIncomeStatement } from "../../company";
 import "./IncomeStatement.css";
+import { useEffect, useState } from "react";
+import { getIncomeStatement } from "../../api";
+import Table from "../Table/Table";
 
 interface Props {}
 
+const configs = [
+  {
+    label: "Date",
+    render: (company: CompanyIncomeStatement) => company.date,
+  },
+  {
+    label: "Revenue",
+    render: (company: CompanyIncomeStatement) => company.revenue,
+  },
+  {
+    label: "Cost Of Revenue",
+    render: (company: CompanyIncomeStatement) => company.costOfRevenue,
+  },
+  {
+    label: "Depreciation",
+    render: (company: CompanyIncomeStatement) =>
+      company.depreciationAndAmortization,
+  },
+  {
+    label: "Operating Income",
+    render: (company: CompanyIncomeStatement) => company.operatingIncome,
+  },
+  {
+    label: "Income Before Taxes",
+    render: (company: CompanyIncomeStatement) => company.incomeBeforeTax,
+  },
+  {
+    label: "Net Income",
+    render: (company: CompanyIncomeStatement) => company.netIncome,
+  },
+  {
+    label: "Net Income Ratio",
+    render: (company: CompanyIncomeStatement) => company.netIncomeRatio,
+  },
+  {
+    label: "Earnings Per Share",
+    render: (company: CompanyIncomeStatement) => company.eps,
+  },
+  {
+    label: "Earnings Per Diluted",
+    render: (company: CompanyIncomeStatement) => company.epsdiluted,
+  },
+  {
+    label: "Gross Profit Ratio",
+    render: (company: CompanyIncomeStatement) => company.grossProfitRatio,
+  },
+  {
+    label: "Opearting Income Ratio",
+    render: (company: CompanyIncomeStatement) => company.operatingIncomeRatio,
+  },
+  {
+    label: "Income Before Taxes Ratio",
+    render: (company: CompanyIncomeStatement) => company.incomeBeforeTaxRatio,
+  },
+];
+
 const IncomeStatement = (props: Props) => {
-  return <div>IncomeStatement</div>;
+  const ticker = useOutletContext<string>();
+  const [incomeStatement, setIncomeStatement] = useState<
+    CompanyIncomeStatement[]
+  >([]);
+  useEffect(() => {
+    const incomeStatementFetch = async () => {
+      const result = await getIncomeStatement(ticker);
+      if (typeof result !== "string") {
+        setIncomeStatement(result!.data);
+      }
+    };
+    incomeStatementFetch();
+  }, []);
+  return (
+    <>
+      {incomeStatement ? (
+        <>
+          <Table configs={configs} data={incomeStatement} />
+        </>
+      ) : (
+        <>Loading...</>
+      )}
+    </>
+  );
 };
 
 export default IncomeStatement;
