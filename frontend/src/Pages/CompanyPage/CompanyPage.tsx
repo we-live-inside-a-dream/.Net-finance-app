9bcf4de437099ed15a202c923897c9db4ca4977f
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2024-09-24 14:42:35 -0600

    feat: included DCF tile and company description

--- frontend/src/Pages/CompanyPage/CompanyPage.tsx
+++ frontend/src/Pages/CompanyPage/CompanyPage.tsx
@@ -33,6 +33,10 @@ const CompanyPage = (props: Props) => {
             <Tile title="Company Name" subTitle={company.companyName} />
             <Tile title="Price" subTitle={company.price.toString()} />
             <Tile title="Sector" subTitle={company.sector} />
+            <Tile title="DCF" subTitle={company.dcf.toString()} />
+            <p className="bg-white shadow rounded text-medium text-gray-900 p-3 mt-1 m-4">
+              {company.description}
+            </p>
           </CompanyDashboard>
         </div>
       ) : (
