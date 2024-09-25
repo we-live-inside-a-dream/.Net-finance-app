5158f1e3fdb248a930d921b691273912ac1c6009
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2024-09-25 09:57:47 -0600

    feat: brought in UseCors

--- backend/api/Program.cs
+++ backend/api/Program.cs
@@ -106,6 +106,12 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors(x => x
+.AllowAnyMethod()
+.AllowAnyHeader()
+.AllowCredentials()
+// .WithOrigins("https://localhost:5084")
+.SetIsOriginAllowed(origin => true));
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
