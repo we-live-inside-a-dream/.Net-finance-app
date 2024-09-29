735f5c22c2da3b0fb1baa16ebe37a4a3bfbda0e1
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2024-09-29 14:10:58 -0600

    fix: import

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router";
 import "./App.css";
-import "react-toastify/dist/ReactTostify.css";
+import "react-toastify/dist/ReactToastify.css";
 import Navbar from "./Components/Navbar/Navbar";
 import { ToastContainer } from "react-toastify";
 import { UserProvider } from "./Context/useAuth";
